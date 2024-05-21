dc0dd7eca36fa833c3f6ab892fd4904462f485ea
parkChuu <pty52@naver.com>
2024-05-21 22:53:07 +0900

    콧물나온다

---
+++ KeeperManegement.java
@@ -0,0 +1,13 @@
+package animal_project;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class KeeperManegement extends JPanel {
+	public KeeperManegement() {
+		JLabel label = new JLabel("사육사 관리");
+		// 전체에 추가
+		add(label);
+		
+}
+}
